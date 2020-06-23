0c8c2ad46ddb78a57e116dd61c90ff2b784e230c
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-06-23 23:37:06 +0300

    finished page routing

--- restore/src/components/pages/home-page.js
+++ restore/src/components/pages/home-page.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import BookList from '../book-list';
 
 const HomePage = () => {
-    return <div>Home</div>
+    const books = [
+        {
+            id: 1,
+            title: "Road to Learn React",
+            author: 'Robert Wieruch'
+        },
+        {
+            id: 2,
+            title: "Road to Learn React",
+            author: 'Robert Wieruch'
+        }
+    ]
+    return (
+        <BookList books={books} />
+    );
 };
 
 export default HomePage;
