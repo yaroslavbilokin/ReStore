eb97e30dc0285772822b4cc6e6574ab76fc999d2
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-07-07 10:40:30 +0300

    added dispatch actions

--- restore/src/components/book-list/book-list.js
+++ restore/src/components/book-list/book-list.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 import './book-list.css'
 import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
+import { withBookstoreService } from '../hoc';
+import { booksLoaded } from '../../actions';
+import { compose } from '../../utils';
 
 class BookList extends Component {
+
+    componentDidMount() {
+        // 1. receive data
+        const { bookstoreService } = this.props;
+        const data = bookstoreService.getBooks();
+
+        // 2. dispatch action to store
+        this.props.booksLoaded(data);
+    }
     render() {
         const { books } = this.props;
         return (
@@ -24,4 +36,11 @@ const mapStateToProps = ({ books }) => {
     return { books };
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = {
+    booksLoaded
+};
+
+export default compose(
+    withBookstoreService(),
+    connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
--- restore/src/reducers/index.js
+++ restore/src/reducers/index.js
@@ -1,16 +1,5 @@
 const initialState = {
-    books: [
-        {
-            id: 1,
-            title: "Road to Learn React",
-            author: 'Robert Wieruch'
-        },
-        {
-            id: 2,
-            title: "Road to Learn React",
-            author: 'Robert Wieruch'
-        }
-    ]
+    books: []
 };
 
 const reducer = (state = initialState, action) => {
