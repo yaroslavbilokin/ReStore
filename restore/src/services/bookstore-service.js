f9d59d5caf2e8bbbfe21520a36a24b72c41fa4cd
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-07-07 11:29:20 +0300

    added styles

--- restore/src/services/bookstore-service.js
+++ restore/src/services/bookstore-service.js
@@ -4,12 +4,16 @@ export default class Bookstoreservice {
             {
                 id: 1,
                 title: "Road to Learn React",
-                author: 'Robert Wieruch'
+                author: 'Robert Wieruch',
+                price: 32,
+                coverImage: 'https://www.kodges.ru/uploads/posts/2018-08/1534871606_the-road-to-learn-react.jpg'
             },
             {
                 id: 2,
                 title: "Road to Learn React",
-                author: 'Robert Wieruch'
+                author: 'Robert Wieruch',
+                price: 32,
+                coverImage: 'https://www.kodges.ru/uploads/posts/2018-08/1534871606_the-road-to-learn-react.jpg'
             }
         ];
     }
