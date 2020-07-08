719faf763baf7a087084189584ed3f9492962e78
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-07-08 21:12:52 +0300

    fixed loading

--- restore/src/components/book-list/book-list.js
+++ restore/src/components/book-list/book-list.js
@@ -3,7 +3,7 @@ import './book-list.css'
 import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
 import { withBookstoreService } from '../hoc';
-import { booksLoaded } from '../../actions';
+import { booksLoaded, booksRequested } from '../../actions';
 import { compose } from '../../utils';
 import Spinner from '../spinner';
 
@@ -11,7 +11,8 @@ class BookList extends Component {
 
     componentDidMount() {
         // 1. receive data
-        const { bookstoreService, booksLoaded } = this.props;
+        const { bookstoreService, booksLoaded, booksRequested } = this.props;
+        booksRequested();
         bookstoreService.getBooks()
             .then((data) => booksLoaded(data));
     }
@@ -42,7 +43,8 @@ const mapStateToProps = ({ books, loading }) => {
 };
 
 const mapDispatchToProps = {
-    booksLoaded
+    booksLoaded,
+    booksRequested
 };
 
 export default compose(
