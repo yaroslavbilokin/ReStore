2b6ee5cf6a05c3ad06e6e31ed41758f3ae9cffea
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-07-08 21:26:21 +0300

    added simple error handling

--- restore/src/components/book-list/book-list.js
+++ restore/src/components/book-list/book-list.js
@@ -3,24 +3,38 @@ import './book-list.css'
 import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
 import { withBookstoreService } from '../hoc';
-import { booksLoaded, booksRequested } from '../../actions';
+import { booksLoaded, booksRequested, booksError } from '../../actions';
 import { compose } from '../../utils';
 import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator'
 
 class BookList extends Component {
 
     componentDidMount() {
         // 1. receive data
-        const { bookstoreService, booksLoaded, booksRequested } = this.props;
+        const {
+            bookstoreService,
+            booksLoaded,
+            booksRequested,
+            booksError
+        } = this.props;
         booksRequested();
         bookstoreService.getBooks()
-            .then((data) => booksLoaded(data));
+            .then((data) => booksLoaded(data))
+            .catch((error) => booksError(error));
     }
     render() {
-        const { books, loading } = this.props;
+
+        const { books, loading, error } = this.props;
+
         if (loading) {
             return <Spinner />
         }
+
+        if (error) {
+            return <ErrorIndicator />
+        }
+
         return (
             <ul className="book-list">
                 {
@@ -35,16 +49,18 @@ class BookList extends Component {
     }
 };
 
-const mapStateToProps = ({ books, loading }) => {
+const mapStateToProps = ({ books, loading, error }) => {
     return {
         books,
-        loading
+        loading,
+        error
     };
 };
 
 const mapDispatchToProps = {
     booksLoaded,
-    booksRequested
+    booksRequested,
+    booksError
 };
 
 export default compose(
--- restore/src/services/bookstore-service.js
+++ restore/src/services/bookstore-service.js
@@ -16,9 +16,13 @@ export default class Bookstoreservice {
         }
     ];
     getBooks() {
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
             setTimeout(() => {
-                resolve(this.data)
+                if (Math.random() > 0.75) {
+                    reject(new Error('Smth bad happene...'))
+                } else {
+                    resolve(this.data)
+                }
             }, 700);
         });
     }
